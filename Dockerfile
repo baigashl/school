0786f36548af03fd6b4a498e86fbf06459a811c3
isa <user@isa.local>
2023-06-30 17:57:53 +0600

    fix

--- Dockerfile
+++ Dockerfile
@@ -8,9 +8,9 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
-RUN python manage.py makemigrations
-RUN python manage.py migrate
 
 COPY . .
+
+RUN python manage.py makemigrations
 
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- school/settings.py
+++ school/settings.py
@@ -84,11 +84,14 @@ WSGI_APPLICATION = 'school.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'school_db',
+        'USER': 'school_user',
+        'PASSWORD': 'school_userSmart1',
+        'HOST': 'db',  # Use the service name defined in docker-compose.yml
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
